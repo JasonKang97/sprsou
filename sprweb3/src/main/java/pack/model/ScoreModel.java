3711ce873d2954a85c1ab5e093412dcaa021d22c
JS <shsh2048@naver.com>
2025-04-16 17:59:58 +0900

    25/04/16

--- sprweb3/src/main/java/pack/model/ScoreModel.java
+++ sprweb3/src/main/java/pack/model/ScoreModel.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class ScoreModel {
 	public String score(ScoreBean bean) {
 		String data = bean.getName() + "님의 총점은 " + (bean.getJava()+bean.getPython()) + "점, 평균은 "
-					+ (bean.getJava()+bean.getPython())/2 + "점입니다." ;
+					+ (double)(bean.getJava()+bean.getPython())/2 + "점입니다." ;
 		return data;
 	}
 }
