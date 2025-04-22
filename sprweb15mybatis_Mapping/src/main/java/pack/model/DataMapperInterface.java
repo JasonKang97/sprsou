fd0d69c0de83b76b7ef5c71532eccb2137b9f4d9
JS <shsh2048@naver.com>
2025-04-22 17:51:13 +0900

    25/04/22

---
+++ sprweb15mybatis_Mapping/src/main/java/pack/model/DataMapperInterface.java
@@ -0,0 +1,12 @@
+package pack.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DataMapperInterface {
+	// 추상 메소드의 이름은 Mapper.xml의 id명과 일치시킨다.
+	List<Board> selectDataAll();
+	
+}
